a03b31093641724368966403316b18b47af5b85c
fitaa4322 <fitaastri55@gmail.com>
2025-05-22 11:20:25 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,45 +1,39 @@
 from flask import Flask, render_template, request, redirect, flash
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # diperlukan untuk flash message
 
-# Konfigurasi koneksi database Railway
-app.config['MYSQL_HOST'] = 'shinkansen.proxy.rlwy.net'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'BzUobQyvRWDoeVzGQpDIvsRpdkfwqIns'
-app.config['MYSQL_DB'] = 'railway'
-app.config['MYSQL_PORT'] = 58092
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  # biar fetchall jadi dict
-# app.config['MYSQL_CHARSET'] = 'utf8mb4'  # opsional, kalau ada masalah encoding
+# Koneksi database via PyMySQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:BzUobQyvRWDoeVzGQpDIvsRpdkfwqIns@shinkansen.proxy.rlwy.net:58092/railway'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-mysql = MySQL(app)
+db = SQLAlchemy(app)
 
-# Home, tampilkan semua data users
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
 @app.route('/')
 def index():
-    with mysql.connection.cursor() as cur:
-        cur.execute("SELECT * FROM users")
-        users = cur.fetchall()
+    users = User.query.all()
     return render_template('index.html', users=users)
 
-# Form tambah data
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
-
-        # Validasi sederhana
         if not name or not email:
             flash("Nama dan Email harus diisi!")
             return redirect('/add')
 
         try:
-            with mysql.connection.cursor() as cur:
-                cur.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (name, email))
-                mysql.connection.commit()
+            new_user = User(name=name, email=email)
+            db.session.add(new_user)
+            db.session.commit()
             flash("Data berhasil ditambahkan!")
             return redirect('/')
         except Exception as e:
